08a1863171267f9ce3401b6f70d3917621bd0af1
Daniel Izidio de Lima <daniel.izidioparacontato@gmail.com>
2022-03-25 16:35:41 -0300

    Added logic to get different types of attributes

--- routes/scrap.js
+++ routes/scrap.js
@@ -1,22 +1,45 @@
 import express from "express";
 import puppeteer from 'puppeteer'
+import getSelectorsFromMappedUrl from '../mapped-urls/mapped-url.js'
 
 const router = express.Router();
 
-router.post('/', async (req,res)=>{
-    // console.log(req.body)
-    // res.send(req.body);
-    
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.amazon.com.br/Xiaomi-Vers%C3%A3o-Global-Lacrada-preta/dp/B07V822TVL')
-   
-    const element = await page.waitForSelector('#productTitle')
-    const value = await element.evaluate(el=> el.textContent);
-    console.log('https://www.amazon.com.br/teste'.split('/\//'))
-    await browser.close();
+router.post('/', async (req, res) => {
+    const { url } = req.body;
+    const value = await getProdutcInformation(getSelectorsFromMappedUrl(url), url);
     res.send(JSON.stringify(value))
-
 })
 
+const getProdutcInformation = async (selectors, url) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url)
+
+        for (var key in selectors) {
+            if (selectors.hasOwnProperty(key)) {
+                const element = await page.waitForSelector(selectors[key])
+
+                const value = await element.evaluate((el, key) => {
+                    const properties = {
+                        titulo: el.textContent,
+                        imagem: el.getAttribute('src'),
+                        preco: el.textContent,
+                        descricao: el.textContent,
+                    }
+                    return properties[key];
+                }, key);
+
+                selectors[key] = value;
+            }
+        }
+
+    } catch (error) {
+        console.log('error')
+    } finally {
+        await browser.close();
+    }
+
+    return selectors;
+}
 export default router;
