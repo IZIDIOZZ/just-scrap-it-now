fd9ab4c620a68ace26bb15dde4fe0bc922005f46
Daniel Izidio de Lima <daniel.izidioparacontato@gmail.com>
2022-03-27 16:35:21 -0300

    added cors to api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import scrapRoute from './app/routes.js';
 import startDBConnection from './app/database/index.js';
+import cors from 'cors';
 
 const app  = express();
 
@@ -9,8 +10,8 @@ startDBConnection();
 
 const PORT = 5000;
 
+app.use(cors())
 app.use(bodyParser.json());
-
 app.use('/scrap', scrapRoute);
 
 app.listen(PORT, ()=>console.log(`server running on port: http://localhost:${PORT}`));
