258f93e26d17fb044b1986e009d76e4493cebade
Daniel Izidio de Lima <daniel.izidioparacontato@gmail.com>
2022-03-27 19:01:17 -0300

    added args to lanch puppeteer

--- src/app/services/scrapService.js
+++ src/app/services/scrapService.js
@@ -4,7 +4,12 @@ import mappedSelectorForUrl from "../mapping/mappedSelectorsUrl.js"
 import productRepository from "../repository/productRepository.js";
 
 export const getProductInformation = async (url) => {
-    const browser = await puppeteer.launch();
+    
+    const browser = await puppeteer.launch( {args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ]});
+
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
     const selectors = getSelectorsFromMappedUrl(url);
