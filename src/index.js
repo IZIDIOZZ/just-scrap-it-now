b4d7a51e6f30a94959f6ef6e941f234fef7d51ff
Daniel Izidio de Lima <daniel.izidioparacontato@gmail.com>
2022-03-27 21:41:51 -0300

    added html return fot the get route

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import bodyParser from 'body-parser';
 import scrapRoute from './app/routes.js';
 import startDBConnection from './app/database/index.js';
 import cors from 'cors';
+import path from 'path';
 
-const app  = express();
-
+const app = express();
 startDBConnection();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/scrap', scrapRoute);
 
-app.get('/', (req,res)=>{res.send('Scraper Working Fine');})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(path.resolve(), '/README.html'));
+})
 
-app.listen(process.env.PORT || 3000, ()=>console.log(`server running on port: ${process.env.PORT}`));
+app.listen(process.env.PORT || 3000, () => console.log(`server running on port: ${process.env.PORT}`));
