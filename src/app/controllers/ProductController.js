1983db6d5c8a1f9b8ffa27b374c2c69de717e02d
Daniel Izidio de Lima <daniel.izidioparacontato@gmail.com>
2022-03-27 21:57:39 -0300

    Add validation for empty or null url

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -4,6 +4,12 @@ import Product from "../models/Product.js";
 export default {
     async scrapp(req, res) {
         const { url } = req.body;
+        if (!url) {
+            return res.send({
+                errorMessage: `url must be informed and cannot be empty`
+            })
+        };
+
         const productInformation = await getProductInformation(url);
         if (!requestWasSuccess(productInformation)) await Product.findOne(productInformation);
         return res.send(productInformation);
